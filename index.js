72769925c649078714e1947123e40b25373adfa9
minhkhoa22 <minhkhoa090201@gmail.com>
2024-12-05 00:26:38 +0700

    Bổ sung chức năng chuyển tab

---
+++ index.js
@@ -0,0 +1,45 @@
+const tabGroups = document.querySelectorAll("[data-tab-group]");
+  tabGroups.forEach((tabGroup) => {
+    const tabsNav = tabGroup.querySelector("[data-tab-nav]");
+    const tabsNavItem = tabsNav.querySelectorAll("[data-tab]");
+
+   
+    const activeTabName =
+      localStorage.getItem(`activeTabName-${tabGroup.dataset.tabGroup}`) ||
+      tabsNavItem[0].getAttribute("data-tab");
+
+    setActiveTab(tabGroup, activeTabName);
+
+    tabsNavItem.forEach((tabNavItem) => {
+      tabNavItem.addEventListener("click", (e) => {
+        e.preventDefault();
+        const tabName = tabNavItem.dataset.tab;
+        setActiveTab(tabGroup, tabName);
+
+        localStorage.setItem(
+          `activeTabName-${tabGroup.dataset.tabGroup}`,
+          tabName
+        );
+      });
+    });
+  });
+
+  function setActiveTab(tabGroup, tabName) {
+    const tabsNav = tabGroup.querySelector("[data-tab-nav]");
+    const tabsContent = tabGroup.querySelector("[data-tab-content]");
+
+    tabsNav.querySelectorAll("[data-tab]").forEach((tabNavItem) => {
+      tabNavItem.classList.remove("active");
+    });
+    tabsContent.querySelectorAll("[data-tab-panel]").forEach((tabPane) => {
+      tabPane.classList.remove("active");
+    });
+
+   
+    const selectedTabNavItem = tabsNav.querySelector(`[data-tab="${tabName}"]`);
+    selectedTabNavItem.classList.add("active");
+    const selectedTabPane = tabsContent.querySelector(
+      `[data-tab-panel="${tabName}"]`
+    );
+    selectedTabPane.classList.add("active");
+  }
