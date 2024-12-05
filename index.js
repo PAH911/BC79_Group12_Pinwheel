94c336df6a01cab9dd661251ffe3886f52a80f47
minhkhoa22 <minhkhoa090201@gmail.com>
2024-12-05 19:59:16 +0700

    Thêm chức năng xem video

--- index.js
+++ index.js
@@ -1,45 +1,53 @@
 const tabGroups = document.querySelectorAll("[data-tab-group]");
-  tabGroups.forEach((tabGroup) => {
-    const tabsNav = tabGroup.querySelector("[data-tab-nav]");
-    const tabsNavItem = tabsNav.querySelectorAll("[data-tab]");
+tabGroups.forEach((tabGroup) => {
+  const tabsNav = tabGroup.querySelector("[data-tab-nav]");
+  const tabsNavItem = tabsNav.querySelectorAll("[data-tab]");
 
-   
-    const activeTabName =
-      localStorage.getItem(`activeTabName-${tabGroup.dataset.tabGroup}`) ||
-      tabsNavItem[0].getAttribute("data-tab");
+  const activeTabName =
+    localStorage.getItem(`activeTabName-${tabGroup.dataset.tabGroup}`) ||
+    tabsNavItem[0].getAttribute("data-tab");
 
-    setActiveTab(tabGroup, activeTabName);
+  setActiveTab(tabGroup, activeTabName);
 
-    tabsNavItem.forEach((tabNavItem) => {
-      tabNavItem.addEventListener("click", (e) => {
-        e.preventDefault();
-        const tabName = tabNavItem.dataset.tab;
-        setActiveTab(tabGroup, tabName);
+  tabsNavItem.forEach((tabNavItem) => {
+    tabNavItem.addEventListener("click", (e) => {
+      e.preventDefault();
+      const tabName = tabNavItem.dataset.tab;
+      setActiveTab(tabGroup, tabName);
 
-        localStorage.setItem(
-          `activeTabName-${tabGroup.dataset.tabGroup}`,
-          tabName
-        );
-      });
+      localStorage.setItem(
+        `activeTabName-${tabGroup.dataset.tabGroup}`,
+        tabName
+      );
     });
   });
+});
 
-  function setActiveTab(tabGroup, tabName) {
-    const tabsNav = tabGroup.querySelector("[data-tab-nav]");
-    const tabsContent = tabGroup.querySelector("[data-tab-content]");
+function setActiveTab(tabGroup, tabName) {
+  const tabsNav = tabGroup.querySelector("[data-tab-nav]");
+  const tabsContent = tabGroup.querySelector("[data-tab-content]");
 
-    tabsNav.querySelectorAll("[data-tab]").forEach((tabNavItem) => {
-      tabNavItem.classList.remove("active");
-    });
-    tabsContent.querySelectorAll("[data-tab-panel]").forEach((tabPane) => {
-      tabPane.classList.remove("active");
-    });
+  tabsNav.querySelectorAll("[data-tab]").forEach((tabNavItem) => {
+    tabNavItem.classList.remove("active");
+  });
+  tabsContent.querySelectorAll("[data-tab-panel]").forEach((tabPane) => {
+    tabPane.classList.remove("active");
+  });
 
-   
-    const selectedTabNavItem = tabsNav.querySelector(`[data-tab="${tabName}"]`);
-    selectedTabNavItem.classList.add("active");
-    const selectedTabPane = tabsContent.querySelector(
-      `[data-tab-panel="${tabName}"]`
-    );
-    selectedTabPane.classList.add("active");
-  }
+  const selectedTabNavItem = tabsNav.querySelector(`[data-tab="${tabName}"]`);
+  selectedTabNavItem.classList.add("active");
+  const selectedTabPane = tabsContent.querySelector(
+    `[data-tab-panel="${tabName}"]`
+  );
+  selectedTabPane.classList.add("active");
+}
+
+// play video
+
+const videoPlayBtn = document.querySelector(".video-play-btn");
+if (videoPlayBtn) {
+  videoPlayBtn.addEventListener("click", function () {
+    const videoPlayer = this.closest(".video").querySelector(".video-player");
+    videoPlayer.classList.remove("hidden");
+  });
+}
